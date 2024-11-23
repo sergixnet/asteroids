4cff966b89c3e399db32890f16cba51fbdc7eabf
Sergio Peña <sergiop79@gmail.com>
2024-11-23 11:08:41 +0100

    Moving player

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,7 +20,11 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
     
     def rotate(self, dt):
-        self.rotation += dt * 100
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -29,3 +33,8 @@ class Player(CircleShape):
             self.rotate(+dt)
         if keys[pygame.K_d]:
             self.rotate(-dt)
+
+        if keys[pygame.K_w]:
+            self.move(+dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
